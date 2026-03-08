4079436da47899f4ae74a0bddff8111914811f7a
Pierre GP <pierregp@MacBook-Pro-de-Pierre.local>
2026-03-08 16:26:22 +0100

    test: add an integration test with dafalgan

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,5 +1,6 @@
 import { Drug, Pharmacy } from "./pharmacy";
 import expectedOutput from "./output.json";
+import expectedOutputWithDafalgan from "./outputWithDafalgan.json";
 
 describe("Pharmacy", () => {
   it("stores the provided drugs", () => {
@@ -43,5 +44,23 @@ describe("Pharmacy", () => {
     }
 
     expect({ result }).toEqual(expectedOutput);
+  });
+
+  it("matches the simulation output when Dafalgan is included", () => {
+    const drugs = [
+      new Drug("Doliprane", 20, 30),
+      new Drug("Herbal Tea", 10, 5),
+      new Drug("Fervex", 12, 35),
+      new Drug("Magic Pill", 15, 40),
+      new Drug("Dafalgan", 10, 20),
+    ];
+    const pharmacy = new Pharmacy(drugs);
+    const result = [];
+
+    for (let elapsedDays = 0; elapsedDays < 30; elapsedDays += 1) {
+      result.push(JSON.parse(JSON.stringify(pharmacy.updateBenefitValue())));
+    }
+
+    expect({ result }).toEqual(expectedOutputWithDafalgan);
   });
 });
